e36e7c45ca6f83edd79fc0033aa0ae8d0272fef1
duribef <diego.uribe.f@gmail.com>
2022-02-06 15:44:20 -0300

    sdfs

--- vars/integracioncontinua.groovy
+++ vars/integracioncontinua.groovy
@@ -1,5 +1,3 @@
-import steps.*
-
 def call(Map args) {
     pipeline {
         agent any
@@ -9,149 +7,165 @@ def call(Map args) {
             STAGE = ' '
         }
         parameters {
-            string  name: 'stages', description: 'Ingrese los stages para ejecutar', trim: true
+            string  name: 'What stage do you want to run now?', description: 'Ingrese los stages para ejecutar', trim: true
         }
         stages {
-            logs.fxLogs()
-            // stage('-1 logs') {
-            //     steps {
-            //         //- Generar análisis con sonar para cada ejecución
-            //         //- Cada ejecución debe tener el siguiente formato de nombre:
-            //         //- {nombreRepo}-{rama}-{numeroEjecucion} ejemplo:
-            //         //- ms-iclab-feature-estadomundial(Si está usando el CRUD ms-iclab-feature-[nombre de su crud])
-            //         script {
-            //             env.GIT_REPO_NAME = env.GIT_URL.replaceFirst(/^.*\/([^\/]+?).git$/, '$1')
-            //             currentBuild.displayName = GIT_REPO_NAME + '-' + BRANCH_NAME + '-' + BUILD_NUMBER
-            //         }
-            //         sh "echo 'branchname: '" + BRANCH_NAME
-            //             script { STAGE = '-1 logs ' }
-            //     }
-            // }
-            stage('Validate mvn') {
-                when {
-                        anyOf {
-                                not { expression { fileExists ('pom.xml') } }
-                                not { expression { fileExists ('mvnw') } }
-                        }
+            stage('-1 logs') {
+                steps {
+                    //- Generar análisis con sonar para cada ejecución
+                    //- Cada ejecución debe tener el siguiente formato de nombre:
+                    //- {nombreRepo}-{rama}-{numeroEjecucion} ejemplo:
+                    //- ms-iclab-feature-estadomundial(Si está usando el CRUD ms-iclab-feature-[nombre de su crud])
+                    script {
+                        env.GIT_REPO_NAME = env.GIT_URL.replaceFirst(/^.*\/([^\/]+?).git$/, '$1')
+                        currentBuild.displayName = GIT_REPO_NAME + '-' + BRANCH_NAME + '-' + BUILD_NUMBER
+                    }
+                    sh "echo 'branchname: '" + BRANCH_NAME
+                        script { STAGE = '-1 logs ' }
                 }
+            }
+            stage('Input') {
+                steps {
+                    script {
+                            def listStagesOrder = [
+                                'build': 'sBuild',
+                                'sonar': 'sSonar',
+                                'run_spring_curl': 'sCurlSpring',
+                                'upload_nexus': 'sUNexus',
+                                'download_nexus': 'sDNexus',
+                                'run_jar': 'sTestJar',
+                                'curl_jar': 'sCurlJar'
+                                ]
+                                stagesArray = searchKeyInArray(params.stages, ';', listStagesOrder)
+                    }
+                    sh "echo 'stagesArray: '"
+                }
+            }
+            // stage('Validate mvn') {
+            //     when {
+            //             anyOf {
+            //                     not { expression { fileExists ('pom.xml') } }
+            //                     not { expression { fileExists ('mvnw') } }
+            //             }
+            //     }
 
-                    steps {
-                        sh "echo  'Faltan archivos Maven en su estructura'"
-                        script {
-                            STAGE = 'Validate Maven Files '
-                            error('file dont exist :( ')
-                        }
-                    }
-            }
-            stage('Update POM') {
-                //- Este stage sólo debe estar disponible para la rama develop.
-                //- Upgrade version del pom.xml si corre develop
-                when {
-                    branch 'develop*'
-                }
-                steps {
-                    sh "echo 'mvnUpdatePom'"
-                    script {
-                        STAGE = 'Update POM '
-                        sh 'mvn versions:set -DnewVersion=1.0.0'
-                    }
-                }
-            }
-            stage('Compile') {
-                //- Compilar el código con comando maven
-                steps {
-                    script { STAGE = 'Compile ' }
-                    sh "echo 'Compile Code!'"
-                    // Run Maven on a Unix agent.
-                    sh 'mvn clean compile -e'
-                }
-            }
-            stage('Unit Test') {
-                //- Testear el código con comando maven
-                steps {
-                    script { STAGE = 'Unit Test ' }
-                    sh "echo 'Test Code!'"
-                    // Run Maven on a Unix agent.
-                    sh 'mvn clean test -e'
-                }
-            }
-            stage('Build jar') {
-                //- Generar artefacto del código compilado.
-                steps {
-                    script { STAGE = 'Build jar ' }
-                    sh "echo 'Build .Jar!'"
-                    // Run Maven on a Unix agent.
-                    sh 'mvn clean package -e'
-                }
-            }
-            stage('SonarQube') {
-                steps {
-                    script { STAGE = 'SonarQube ' }
-                    sh "echo 'SonarQube'"
-                    withSonarQubeEnv('sonarqube') {
-                        sh "echo 'SonarQube'"
-                        sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=covid-devops'
-                    }
-                }
-            // post {
-            //     //- Subir el artefacto creado al repositorio privado de Nexus.
-            //     //- Ejecutar este paso solo si los pasos anteriores se ejecutan de manera correcta.
-            //     success {
-            //         script { STAGE = 'Subir a Nexus ' }
-            //         sh "echo 'Subir a nexus'"
-            //         nexusPublisher nexusInstanceId: 'nexus',
-            //                              nexusRepositoryId: 'ms-iclab',
-            //                             packages: [[$class: 'MavenPackage',
-            //                                  mavenAssetList: [[classifier: '',
-            //                                                  extension: '',
-            //                                                  filePath: 'build/DevOpsUsach2020-${POM_VERSION}.jar']],
-            //                                  mavenCoordinate: [artifactId: 'DevOpsUsach2020',
-            //                                                  groupId: 'com.devopsusach2020',
-            //                                                  packaging: 'jar',
-            //                                                  version: ${POM_VERSION}]]]
-            //     }
-            // }
-            }
-            stage('Nexus') {
-                //- Subir el artefacto creado al repositorio privado de Nexus.
-                //- Ejecutar este paso solo si los pasos anteriores se ejecutan de manera correcta.
-                steps {
-                    script {
-                        STAGE = 'Subir a Nexus '
-                        mavenPom = readMavenPom file: 'pom.xml'
-                        sh "echo ${mavenPom.version}"
-                    }
-                    sh "echo 'Subir a nexus'"
-                    nexusPublisher nexusInstanceId: 'nexus',
-                                     nexusRepositoryId: 'ms-iclab',
-                                    packages: [[$class: 'MavenPackage',
-                                                mavenAssetList: [[classifier: '',
-                                                                extension: '',
-                                                                filePath: "build/DevOpsUsach2020-${mavenPom.version}.jar"]],
-                                                mavenCoordinate: [artifactId: 'DevOpsUsach2020',
-                                                                groupId: 'com.devopsusach2020',
-                                                                packaging: 'jar',
-                                                                version: "${mavenPom.version}"]]]
-                }
-            }
-            stage('Push Develop') {
-                when {
-                    branch 'develop*'
-                }
-                steps {
-                        sh "echo 'Push Develop'"
-                        script {
-                            STAGE = 'gitPushDevelop '
-                            sh "echo 'gitPushDevelop'"
-                        }
-                        withCredentials([gitUsernamePassword(credentialsId: 'github-token')]) {
-                            sh '''
-                            git commit -am "Update pom.xml"
-                            git push origin develop
-                            '''
-                        }
-                }
-            }
+        //         steps {
+        //             sh "echo  'Faltan archivos Maven en su estructura'"
+        //             script {
+        //                 STAGE = 'Validate Maven Files '
+        //                 error('file dont exist :( ')
+        //             }
+        //         }
+        // }
+        // stage('Update POM') {
+        //     //- Este stage sólo debe estar disponible para la rama develop.
+        //     //- Upgrade version del pom.xml si corre develop
+        //     when {
+        //         branch 'develop*'
+        //     }
+        //     steps {
+        //         sh "echo 'mvnUpdatePom'"
+        //         script {
+        //             STAGE = 'Update POM '
+        //             sh 'mvn versions:set -DnewVersion=1.0.0'
+        //         }
+        //     }
+        // }
+        // stage('Compile') {
+        //     //- Compilar el código con comando maven
+        //     steps {
+        //         script { STAGE = 'Compile ' }
+        //         sh "echo 'Compile Code!'"
+        //         // Run Maven on a Unix agent.
+        //         sh 'mvn clean compile -e'
+        //     }
+        // }
+        // stage('Unit Test') {
+        //     //- Testear el código con comando maven
+        //     steps {
+        //         script { STAGE = 'Unit Test ' }
+        //         sh "echo 'Test Code!'"
+        //         // Run Maven on a Unix agent.
+        //         sh 'mvn clean test -e'
+        //     }
+        // }
+        // stage('Build jar') {
+        //     //- Generar artefacto del código compilado.
+        //     steps {
+        //         script { STAGE = 'Build jar ' }
+        //         sh "echo 'Build .Jar!'"
+        //         // Run Maven on a Unix agent.
+        //         sh 'mvn clean package -e'
+        //     }
+        // }
+        // stage('SonarQube') {
+        //     steps {
+        //         script { STAGE = 'SonarQube ' }
+        //         sh "echo 'SonarQube'"
+        //         withSonarQubeEnv('sonarqube') {
+        //             sh "echo 'SonarQube'"
+        //             sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=covid-devops'
+        //         }
+        //     }
+        // // post {
+        // //     //- Subir el artefacto creado al repositorio privado de Nexus.
+        // //     //- Ejecutar este paso solo si los pasos anteriores se ejecutan de manera correcta.
+        // //     success {
+        // //         script { STAGE = 'Subir a Nexus ' }
+        // //         sh "echo 'Subir a nexus'"
+        // //         nexusPublisher nexusInstanceId: 'nexus',
+        // //                              nexusRepositoryId: 'ms-iclab',
+        // //                             packages: [[$class: 'MavenPackage',
+        // //                                  mavenAssetList: [[classifier: '',
+        // //                                                  extension: '',
+        // //                                                  filePath: 'build/DevOpsUsach2020-${POM_VERSION}.jar']],
+        // //                                  mavenCoordinate: [artifactId: 'DevOpsUsach2020',
+        // //                                                  groupId: 'com.devopsusach2020',
+        // //                                                  packaging: 'jar',
+        // //                                                  version: ${POM_VERSION}]]]
+        // //     }
+        // // }
+        // }
+        // stage('Nexus') {
+        //     //- Subir el artefacto creado al repositorio privado de Nexus.
+        //     //- Ejecutar este paso solo si los pasos anteriores se ejecutan de manera correcta.
+        //     steps {
+        //         script {
+        //             STAGE = 'Subir a Nexus '
+        //             mavenPom = readMavenPom file: 'pom.xml'
+        //             sh "echo ${mavenPom.version}"
+        //         }
+        //         sh "echo 'Subir a nexus'"
+        //         nexusPublisher nexusInstanceId: 'nexus',
+        //                          nexusRepositoryId: 'ms-iclab',
+        //                         packages: [[$class: 'MavenPackage',
+        //                                     mavenAssetList: [[classifier: '',
+        //                                                     extension: '',
+        //                                                     filePath: "build/DevOpsUsach2020-${mavenPom.version}.jar"]],
+        //                                     mavenCoordinate: [artifactId: 'DevOpsUsach2020',
+        //                                                     groupId: 'com.devopsusach2020',
+        //                                                     packaging: 'jar',
+        //                                                     version: "${mavenPom.version}"]]]
+        //     }
+        // }
+        // stage('Push Develop') {
+        //     when {
+        //         branch 'develop*'
+        //     }
+        //     steps {
+        //             sh "echo 'Push Develop'"
+        //             script {
+        //                 STAGE = 'gitPushDevelop '
+        //                 sh "echo 'gitPushDevelop'"
+        //             }
+        //             withCredentials([gitUsernamePassword(credentialsId: 'github-token')]) {
+        //                 sh '''
+        //                 git commit -am "Update pom.xml"
+        //                 git push origin develop
+        //                 '''
+        //             }
+        //     }
+        // }
         //    stage('Create Release') {
         //        //- Crear rama release cuando todos los stages anteriores estén correctamente ejecutados.
         //        //- Este stage sólo debe estar disponible para la rama develop.
@@ -187,4 +201,21 @@ def call(Map args) {
             }
         }
     }
+}
+
+void searchKeyInArray(String keyWordsAsString, String splitIdentifier, Map arrayMapToCompare) {
+    def _array = []
+    keyWordsAsString.split("${splitIdentifier}").each {
+        def _key = it?.trim()
+        if (!_key.equals('') && ( arrayMapToCompare.containsKey(it) )) {
+            _array.add(arrayMapToCompare[it])
+        }else {
+            //it could be 'error'
+            println('***************************************************************')
+            figlet  " ${it} "
+            println "No se encontró como una función válida, las opociones son:${arrayMapToCompare.keySet() as List}"
+            println('***************************************************************')
+        }
+    }
+    return _array
 }
